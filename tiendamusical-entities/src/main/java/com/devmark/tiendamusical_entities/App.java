005f3ea58aa710d00457c53b90566cb2ed62462e
Marcos Salvador Hernández Guadarrama <marcoshernandezg18@yahoo.com>
2021-07-12 12:45:03 -0500

    Configuración de Spring-Data-JPA, Spring, JSF y PrimeFaces

---
+++ tiendamusical-entities/src/main/java/com/devmark/tiendamusical_entities/App.java
@@ -0,0 +1,13 @@
+package com.devmark.tiendamusical_entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
